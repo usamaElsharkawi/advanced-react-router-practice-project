4a032731be465437207d82cb2c2e71b36694fd01
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-03-22 07:29:46 +0200

    refactor: Move eventsLoader into Events.jsx and clean up App router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, {loader as eventsLoader} from "./pages/Events";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventDetailPage from "./pages/EventDetail";
 import LayoutRoot from "./pages/LayoutRoot";
 import EventsLayout from "./pages/EventsLayout";
+ 
 
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
-async function  eventsLoader() {
-  const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        //...
-      } else {
-        const resData = await response.json();
-        return resData.events;
-      }
-}
-  
 
 const router = createBrowserRouter([
   {
