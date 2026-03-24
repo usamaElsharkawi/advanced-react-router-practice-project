b12f30a9503897453992e6109862d7cf41f03441
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-03-24 06:49:39 +0200

    Behind-the-Scenes Work with useFetcher().

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,68 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import NewEventPage from "./pages/NewEvent";
-import EditEventPage from "./pages/EditEvent";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
-} from "./pages/EventDetail";
-import LayoutRoot from "./pages/LayoutRoot";
-import EventsLayout from "./pages/EventsLayout";
-import ErrorPage from "./pages/Error";
-import { action as manipulateEventAction } from "./components/EventForm";
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsLayout';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/LayoutRoot';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <LayoutRoot />,
+    path: '/',
+    element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "events",
-        element: <EventsLayout />,
+        path: 'events',
+        element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage />, loader: eventsLoader },
-          { path: "new", element: <NewEventPage />, action: manipulateEventAction },
           {
-            path: ":eventId",
-            id: "event-detail",
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ':eventId',
+            id: 'event-detail',
             loader: eventDetailLoader,
             children: [
-              { index: true, element: <EventDetailPage />, action:deleteEventAction },
-              { path: "edit", element: <EditEventPage />, action: manipulateEventAction },
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
             ],
           },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
 ]);
+
 function App() {
   return <RouterProvider router={router} />;
 }
---
+++ frontend/src/components/NewsletterSignup.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import classes from "./NewsletterSignup.module.css";
+import { useFetcher } from "react-router-dom";
+
+function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const {state,data} = fetcher
+
+  useEffect(()=>{
+    if(state === 'idle'&& data && data.message){
+      alert(data.message)
+    }
+  },[state,data])
+
+  return (
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
+      <input
+        name="email"
+        type="email"
+        placeholder="Sign up for newsletter..."
+        aria-label="Sign up for newsletter"
+      />
+      <button>Sign up</button>
+    </fetcher.Form>
+  );
+}
+
+export default NewsletterSignup;
